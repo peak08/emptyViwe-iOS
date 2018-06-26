8c2ddab7bfd67d8afa59c32bf5a22c5b9f344016
peak08 <1558864349@qq.com>
2018-06-26 14:38:10 +0800

    导入

--- dataEmpty/emptyView/emptyView.h
+++ dataEmpty/emptyView/emptyView.h
@@ -8,6 +8,36 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum : NSUInteger {
+    enptyDataType ,
+    enptyNetErrorType,
+} EmptyType;
+
+typedef void(^ActionBlock)(void);
 @interface emptyView : UIView
+
+@property(nonatomic,strong) UIImageView *emptyImageView;
+@property(nonatomic,strong) UILabel *emptyTitle;
+@property(nonatomic,strong) UILabel *emptyDetail;
+@property(nonatomic,strong) UIButton *emptyReloadBut;
+@property(nonatomic,assign) EmptyType emptyType;
+
+
+@property(nonatomic,copy) ActionBlock actionBlock;
+@property(nonatomic,strong) UIColor *emptyBackgroundColor;
+@property(nonatomic,copy) NSString *emptyImageName;
+@property(nonatomic,copy) NSString *emptyDetailStr;
+@property(nonatomic,copy) NSString *emptyReloadButStr;
+@property(nonatomic,copy) NSString *emptyTitleStr;
+
+
++ (instancetype)emptyViewiconName:(NSString *)iconName Title:(NSString *)Title detailTitle:(NSString *)detailTitle actionButtonTitle:(NSString *)actionButtonTitle actionBlock:(void(^)(void))actionBlock; 
+
+-(void)hiddenEmptyView;
+
+
+
+
+
 
 @end
