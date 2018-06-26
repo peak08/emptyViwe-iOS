29adceeeab07f505c73feccac5c3b0a0b429838a
peak08 <1558864349@qq.com>
2018-06-26 14:32:05 +0800

    first commit

---
+++ dataEmpty/emptyView/emptyView.h
@@ -0,0 +1,13 @@
+//
+//  emptyView.h
+//  dataEmpty
+//
+//  Created by apple on 2018/6/26.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface emptyView : UIView
+
+@end
---
+++ dataEmpty/emptyView/emptyView.m
@@ -0,0 +1,21 @@
+//
+//  emptyView.m
+//  dataEmpty
+//
+//  Created by apple on 2018/6/26.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import "emptyView.h"
+
+@implementation emptyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
